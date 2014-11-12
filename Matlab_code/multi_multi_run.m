86e42f2ad3f2abf44b37aa304b413ea6a46804a5
eacappo <eacappo@gmail.com>
2014-11-12 18:52:58 -0500

    Added my results file over 10 trials for regular, +noised, and +random reatures.

--- Matlab_code/multi_multi_run.m
+++ Matlab_code/multi_multi_run.m
@@ -60,9 +60,26 @@ STD_DEV_RANDOM = std(results_random,0,3);
 %%
 clc
 
-save('results_SVM.mat', 'RESULTS_0','STD_DEV_0',...
-                        'RESULTS_NOISE','STD_DEV_NOISE',...
-                        'RESULTS_RANDOM','STD_DEV_RANDOM');
+% save('results_SVM.mat', 'RESULTS_0','STD_DEV_0',...
+%                         'RESULTS_NOISE','STD_DEV_NOISE',...
+%                         'RESULTS_RANDOM','STD_DEV_RANDOM');
 
-toc()                    
+toc()        
 
+RESULTS_NOISE(:,[2,6]) - RESULTS_0(:,[2,6])
+RESULTS_RANDOM(:,[2,6]) - RESULTS_0(:,[2,6])
+
+max_results_dif_noise_mag   = max(max(RESULTS_NOISE(:,[2,6]) - RESULTS_0(:,[2,6])))
+min_results_dif_noise_mag   = min(min(RESULTS_NOISE(:,[2,6]) - RESULTS_0(:,[2,6])))
+
+max_results_dif_random_mag  = max(max(RESULTS_RANDOM(:,[2,6]) - RESULTS_0(:,[2,6])))
+min_results_dif_random_mag  = min(min(RESULTS_RANDOM(:,[2,6]) - RESULTS_0(:,[2,6])))
+
+
+
+% RESULTS_0
+%     0.9596    0.9608    0.7784    0.7938    0.8876    0.8944    0.8859    0.8514
+%     0.9877    0.9881    0.3006    0.3474    0.9488    0.9473    0.6094    0.5983
+%     0.9912    0.9909    0.7269    0.7304    0.9717    0.9715    0.4941    0.5162
+%     0.9886    0.9889    0.9993    0.9861    0.9881    0.9870    0.9858    0.9806
+%     0.9618    0.9637    0.8611    0.8667    0.9266    0.9256    0.7082    0.7084
